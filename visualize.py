87796ae2da2bb8a112fabdc1b50ebdd40108385e
mephody-bro <kirill.fedyanin@gmail.com>
2019-08-23 14:09:53 +0300

    VAE pt.2

--- visualize.py
+++ visualize.py
@@ -1,6 +1,5 @@
 import random
 
-from scipy.optimize import rosen
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -11,9 +10,6 @@ from dataloader.rosen import RosenData
 from uncertainty_estimator.nngp import NNGPRegression
 from uncertainty_estimator.mcdue import MCDUE
 from uncertainty_estimator.random_estimator import RandomEstimator
-from sample_selector.eager import EagerSampleSelector
-from oracle.identity import IdentityOracle
-from al_trainer import ALTrainer
 from analysis.autoencoder import VAE
 
 config = {
@@ -64,12 +60,44 @@ def get_model(retrain, model_path, train_set, val_set):
     return model
 
 
+def get_vae(restore, rosen, patience):
+    x_pool, y_pool = rosen.dataset('pool')
+    x_train, y_train = rosen.dataset('train')
+    x_val, y_val = rosen.dataset('train')
+    x = np.concatenate((x_train, x_pool))
+
+    vae_path = 'model/data/vae.ckpt'
+    vae = VAE(10, 10, 2)
+    if restore:
+        vae.load_state_dict(torch.load(vae_path))
+    else:
+        current_patience = patience
+        best_loss = float('inf')
+
+        for epoch in range(epochs):
+            vae.fit(x)
+            if (epoch+1) % 100 == 0:
+                val_loss = vae.evaluate(x_val)
+                print('{} ====> Val set loss: {:.4f}'.format(epoch+1, val_loss))
+                if val_loss < best_loss:
+                    best_loss = val_loss
+                    current_patience = patience
+                else:
+                    current_patience -= 1
+                    if current_patience <= 0:
+                        print("No patience left")
+                        break
+        torch.save(vae.state_dict(), vae_path)
+    return vae
+
+
 if __name__ == '__main__':
-    epochs = 10000
+    epochs = 100_000
     patience = 5
-    rosen = RosenData(
-        config['n_dim'], config['data_size'], config['data_split'],
-        use_cache=config['use_cache'])
+    # rosen = RosenData(
+    #     config['n_dim'], config['data_size'], config['data_split'],
+    #     use_cache=config['use_cache'])
+    rosen = RosenData(config['n_dim'], 20000, config['data_split'], use_cache=False)
     x_pool, y_pool = rosen.dataset('pool')
     x_train, y_train = rosen.dataset('train')
     x_val, y_val = rosen.dataset('train')
@@ -79,37 +107,37 @@ if __name__ == '__main__':
     estimator = build_estimator(config['estimator'], model)
 
     estimation = estimator.estimate(x_pool, x_train, y_train)
-    # make some encoder-decoder.
-    restore_vae = True
-    vae_path = 'model/data/vae.ckpt'
-    vae = VAE(10, 10, 2)
-    if restore_vae:
-        vae.load_state_dict(torch.load(vae_path))
-    else:
-        patience = 10
-        current_patience = patience
-        best_loss = float('inf')
 
-        for epoch in range(epochs):
-            vae.fit(x_train)
-            if (epoch+1) % 100 == 0:
-                val_loss = vae.evaluate(x_val)
-                print('{} ====> Val set loss: {:.4f}'.format(epoch+1, val_loss))
-                if val_loss < best_loss:
-                    best_loss = val_loss
-                    current_patience = patience
-                else:
-                    patience -= 1
-                    if patience <= 0:
-                        print("No patience left")
-                        break
-        torch.save(vae.state_dict(), vae_path)
+    vae = get_vae(restore=False, rosen=rosen, patience=patience)
 
+    # Check vae
     x_batch = x_train[:30]
-    encoded = vae.predict(x_batch)
+    decoded = vae.predict(x_batch)
     for i in range(5):
-        plt.scatter([x[i] for x in x_batch], [y[i] for y in encoded])
+        plt.scatter([x[i] for x in x_batch], [y[i] for y in decoded])
     plt.show()
+
+
+    # # Make 3d picture of pool points
+    # points = 500
+    # x_batch = x_pool[:points]
+    # vae.eval()
+    # with torch.no_grad():
+    #     surface = vae(torch.DoubleTensor(x_batch))[1].tolist()
+    # xs = [point[0] for point in surface]
+    # ys = [point[1] for point in surface]
+    #
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # Axes3D(fig).scatter(xs, ys, zs=y_pool[:points])
+    # plt.show()
+
+
+
+
+
+
+
 
 
 
