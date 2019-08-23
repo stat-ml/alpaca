3d2eba1a115edfb49d991647934b0ff8c34f511b
mephody-bro <kirill.fedyanin@gmail.com>
2019-08-23 19:04:33 +0300

    Plot error vs uq

--- visualize.py
+++ visualize.py
@@ -29,13 +29,13 @@ config = {
 }
 
 
-def build_estimator(name, model):
+def build_estimator(name, model, **kwargs):
     if name == 'nngp':
-        estimator = NNGPRegression(model)
+        estimator = NNGPRegression(model, **kwargs)
     elif name == 'random':
         estimator = RandomEstimator()
     elif name == 'mcdue':
-        estimator = MCDUE(model)
+        estimator = MCDUE(model, **kwargs)
     else:
         raise ValueError("Wrong estimator name")
     return estimator
@@ -49,10 +49,10 @@ def set_random(random_seed):
         random.seed(random_seed)
 
 
-def get_model(retrain, model_path, train_set, val_set):
+def get_model(retrain, model_path, train_set, val_set, epochs=10000):
     model = MLP(config['layers'])
     if retrain:
-        model.fit(train_set, val_set)
+        model.fit(train_set, val_set, epochs=epochs)
         torch.save(model.state_dict(), model_path)
     else:
         model.load_state_dict(torch.load(model_path))
@@ -97,58 +97,17 @@ if __name__ == '__main__':
     rosen = RosenData(
         config['n_dim'], config['data_size'], config['data_split'],
         use_cache=config['use_cache'])
-    # rosen = RosenData(config['n_dim'], 20000, config['data_split'], use_cache=False)
     x_pool, y_pool = rosen.dataset('pool')
     x_train, y_train = rosen.dataset('train')
     x_val, y_val = rosen.dataset('train')
 
     set_random(config['random_seed'])
-    model = get_model(config['retrain'], config['model_path'], (x_train, y_train), (x_val, y_val))
+    x_full = np.concatenate((x_train, x_pool))
+    y_full = np.concatenate((y_train, y_pool))
+    model = get_model(config['retrain'], config['model_path'], (x_full, y_full), (x_val, y_val))
     estimator = build_estimator(config['estimator'], model)
     estimation = estimator.estimate(x_pool, x_train, y_train)
 
     points = 5
     print(model(x_pool[:points]), y_pool[:points])
-
-    # vae = get_vae(restore=False, rosen=rosen, patience=patience)
-    #
-    # # Check vae
-    # x_batch = x_train[:30]
-    # decoded = vae.predict(x_batch)
-    # for i in range(5):
-    #     plt.scatter([x[i] for x in x_batch], [y[i] for y in decoded])
-    # plt.show()
-    #
-    #
-    # # # Make 3d picture of pool points
-    # # points = 500
-    # # x_batch = x_pool[:points]
-    # # vae.eval()
-    # # with torch.no_grad():
-    # #     surface = vae(torch.DoubleTensor(x_batch))[1].tolist()
-    # # xs = [point[0] for point in surface]
-    # # ys = [point[1] for point in surface]
-    # #
-    # # fig = plt.figure()
-    # # ax = fig.add_subplot(111, projection='3d')
-    # # Axes3D(fig).scatter(xs, ys, zs=y_pool[:points])
-    # # plt.show()
-    #
-    #
-    #
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
