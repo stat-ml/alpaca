6ed75a7b9e2ca004ca7faedbbce01bcc2a7026e2
mephody-bro <kirill.fedyanin@gmail.com>
2019-09-10 18:05:53 +0300

    Clean up nngp debug

--- uncertainty_estimator/nngp.py
+++ uncertainty_estimator/nngp.py
@@ -1,9 +1,5 @@
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-import seaborn as sns
 
 
 class NNGPRegression:
@@ -21,33 +17,12 @@ class NNGPRegression:
 
         # covariance matrix with regularization
         cov_matrix_train = np.cov(mcd_predictions[:train_len, :], ddof=0)
-        print(cov_matrix_train.shape)
-        plt.subplot(4, 2, 3)
-        plt.title('Covariations')
-        sns.heatmap(cov_matrix_train)
-        print('cov_matrix', cov_matrix_train.shape)
-        print(cov_matrix_train[:3, :3])
-
         cov_matrix_inv = np.linalg.inv(cov_matrix_train + np.eye(train_len)*self.diag_eps)
 
         pool_samples = mcd_predictions[train_len:]
         Qs = self.simple_covs(mcd_predictions[:train_len, :], pool_samples).T
-        plt.subplot(4, 2, 4)
-        plt.title('Correlations')
-        sns.heatmap(Qs)
-        print('Qs', Qs.shape)
-        print(Qs[:3, :3])
-
         Qt_K_Q = np.matmul(np.matmul(Qs.T, cov_matrix_inv), Qs)
-        plt.subplot(4, 2, 5)
-        plt.title('QtQs')
-        sns.heatmap(Qt_K_Q)
-        print('QtKQs', Qt_K_Q.shape)
-        print(Qt_K_Q[:3, :3])
-
         KKs = np.var(pool_samples, axis=1)
-        print('KKs', KKs.shape)
-        print(KKs[:3])
 
         ws = KKs - np.diag(Qt_K_Q)
 
@@ -66,10 +41,5 @@ class NNGPRegression:
     def simple_covs(a, b):
         ac = a - a.mean(axis=-1, keepdims=True)
         bc = (b - b.mean(axis=-1, keepdims=True)) / b.shape[-1]
-        # from IPython.core.debugger import set_trace; set_trace()
         return np.dot(ac, bc.T).T
-    #
-    # @staticmethod
-    # def simple_covs(_x, _y):
-    #     return np.mean((_x-np.mean(_x))*(_y-np.mean(_y)), axis = 1)
 
