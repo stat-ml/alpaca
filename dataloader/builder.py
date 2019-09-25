0b9f9411a17991435a4410d5d9586a7db422be86
mephody-bro <kirill.fedyanin@gmail.com>
2019-09-25 16:56:55 +0300

    Naval propulsion dataset

--- dataloader/builder.py
+++ dataloader/builder.py
@@ -1,12 +1,16 @@
 from .boston_housing import BostonHousingData
 from .concrete import ConcreteData
 from .energy_efficiency import EnergyEfficiencyData
+from .kin8nm import Kin8nmData
+from .naval_propulsion import NavalPropulsionData
 
 
 DATASETS = {
     'boston_housing': BostonHousingData,
     'concrete': ConcreteData,
-    'energy_efficiency': EnergyEfficiencyData
+    'energy_efficiency': EnergyEfficiencyData,
+    'kin8nm': Kin8nmData,
+    'naval_propulsion': NavalPropulsionData
 }
 
 
--- dataloader/naval_propulsion.py
+++ dataloader/naval_propulsion.py
@@ -3,20 +3,21 @@ from os import path
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+import zipfile
 
 from experiment_setup import ROOT_DIR
 from .saver import DataSaver
 from .downloader import download
 
 
-URL = 'https://www.openml.org/data/get_csv/3626/dataset_2175_kin8nm.arff'
+URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00316/UCI%20CBM%20Dataset.zip'
 
 
-class Kin8nmData:
-    """Load/provides kin8nm dataset"""
+class NavalPropulsionData:
+    """Load/provides naval propulsion dataset"""
     def __init__(self, use_cache=False, val_split=0.2):
         self.use_cache = use_cache
-        cache_dir = path.join(ROOT_DIR, 'dataloader/data/kin8nm')
+        cache_dir = path.join(ROOT_DIR, 'dataloader/data/naval_propulsion')
         self.saver = DataSaver(cache_dir)
         self.val_split = val_split
         self._build_dataset(cache_dir)
@@ -26,14 +27,16 @@ class Kin8nmData:
             return self.saver.load(label)
 
         data = self.data[label]
-        x, y = data[:, :-1], data[:, -1:]
+        x, y = data[:, :-2], data[:, -1:]
         self.saver.save(x, y, label)
         return x, y
 
     def _build_dataset(self, cache_dir):
-        data_path = download(cache_dir, 'kin8nm.csv', URL)
-        self.df = pd.read_csv(data_path)
-        print(self.df.head())
+        data_path = download(cache_dir, 'naval_propulsion.zip', URL)
+        with zipfile.ZipFile(data_path, 'r') as zip_ref:
+            zip_ref.extractall(cache_dir)
+        file_path = path.join(cache_dir, 'UCI CBM Dataset', 'data.txt')
+        self.df = pd.read_csv(file_path, delim_whitespace=True, header=None)
         table = self.df.to_numpy()
         train, val = train_test_split(table, test_size=self.val_split, shuffle=True)
         self.data = {
@@ -44,7 +47,8 @@ class Kin8nmData:
 
 
 if __name__ == '__main__':
-    dataset = Kin8nmData()
+    dataset = NavalPropulsionData()
     x_train, y_train = dataset.dataset('train')
     x_val, y_val = dataset.dataset('val')
+    print(x_train.shape, y_val.shape)
 
