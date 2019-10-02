e910e610c3436f13e581e90768b1404551c76734
Tismoney <mokrov@frtk.ru>
2019-10-02 16:18:30 +0300

    ADD:  Inheritance

--- uncertainty_estimator/eue.py
+++ uncertainty_estimator/eue.py
@@ -1,19 +1,5 @@
 import numpy as np
 import torch  
-
-class EnsembleUE:
-    """
-    Estimate uncertainty for samples with Ensemble approach
-    """
-    def __init__(self, net):
-        self.net = net
-        
-    def estimate(self, X_pool, *args):
-
-        with torch.no_grad():
-            predictions = self.net(X_pool, reduction=None).to('cpu')
-
-        return np.ravel(predictions.std(dim=0))
     
 class EnsembleMCDUE:
     """
@@ -25,14 +11,22 @@ class EnsembleMCDUE:
         self.dropout_rate = dropout_rate
         self.n_models = self.net.n_models
     
-    def estimate(self, X_pool, *args):
+    def estimate(self, X_pool, **kwargs):
         mcd_realizations = []
 
         with torch.no_grad():
             for nn_run in range(self.nn_runs):
-                prediction = self.net(X_pool, reduction=None, dropout_rate=self.dropout_rate)
-                prediction = np.ravel(prediction.to('cpu')).reshape((X_pool.shape[0], self.n_models))
+                prediction = self.net(X_pool, reduction=None, dropout_rate=self.dropout_rate, **kwargs)
+                prediction = prediction.to('cpu')
                 mcd_realizations.append(prediction)
-
-        mcd_realizations = np.concatenate(mcd_realizations, axis=1)
-        return np.ravel(np.std(mcd_realizations, axis=1))
+        
+        mcd_realizations = torch.cat(mcd_realizations, dim=1)
+        return np.ravel(mcd_realizations.std(dim=1, unbiased=False))
+    
+    
+class EnsembleUE(EnsembleMCDUE):
+    """
+    Estimate uncertainty for samples with Ensemble approach
+    """
+    def __init__(self, net):
+        super(EnsembleUE, self).__init__(net, nn_runs=1, dropout_rate=0)
