ce91ac6c0ff2fb200b519fccda1129274076d30e
tismoney <mokrov@frtk.ru>
2019-10-14 14:32:58 +0300

    UPD variance

--- loss/nll_loss.py
+++ loss/nll_loss.py
@@ -3,12 +3,12 @@ from torch.nn.modules.loss import _Loss
 
 
 class NLLRegLoss(_Loss):
-    def __init__(self, size_average=None, reduce=None, reduction='mean', eps=1e-10):
+    def __init__(self, size_average=None, reduce=None, reduction='mean', eps=1e-6):
         super(NLLRegLoss, self).__init__(size_average, reduce, reduction)
         self.eps = eps
     
     def forward(self, input, target):
-        mu, sigma_sq = input[:, 0], input[:, 1] ** 2
+        mu, sigma = input[:, 0], input[:, 1]
         target = target.reshape_as(mu)
-        output = torch.log(sigma_sq + self.eps)/2 + (target - mu)**2 / (2 * sigma_sq)
+        output = 0.5 * torch.log(sigma + self.eps) + 0.5 * (target - mu)**2 / sigma
         return getattr(output, self.reduction)()
