9839c9e0645ec4ba2123fd663f3eca20170feb23
mephody-bro <kirill.fedyanin@gmail.com>
2019-11-06 17:28:55 +0300

    Prediction

--- model/cnn.py
+++ model/cnn.py
@@ -34,7 +34,12 @@ class Trainer:
                         epoch, batch_idx * len(data), len(loader.dataset),
                                    100. * batch_idx / len(loader), loss.item()))
 
-    def score(self, ):
+    def predict(self, x):
+        self.model.eval()
+        x = torch.FloatTensor(x).to(self.device)
+        with torch.no_grad():
+            predictions = self.model(x).argmax(dim=1, keepdim=True)
+        return predictions.cpu().numpy()
 
     def _to_loader(self, x, y, shuffle=False):
         ds = TensorDataset(torch.FloatTensor(x), torch.LongTensor(y.reshape(-1)))
@@ -74,23 +79,4 @@ def train(config, model, device, train_loader, optimizer, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
-
-
-def test(config, model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
 
