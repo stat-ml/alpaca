9fe6af1e6ba8dd6d88d3b7443a866a5c43fc9abd
mephody-bro <kirill.fedyanin@gmail.com>
2019-11-12 14:32:05 +0300

    Plot for different train sizes

--- model/cnn.py
+++ model/cnn.py
@@ -61,12 +61,12 @@ class Trainer:
 class SimpleConv(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3)
-        self.conv2 = nn.Conv2d(32, 64, 3)
-        self.linear_size = 12*12*64
-        self.fc1 = nn.Linear(self.linear_size, 128)
+        self.conv1 = nn.Conv2d(1, 16, 3)
+        self.conv2 = nn.Conv2d(16, 32, 3)
+        self.linear_size = 12*12*32
+        self.fc1 = nn.Linear(self.linear_size, 256)
         self.dropout = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x, dropout_rate=0., dropout_mask=None):
         x = F.elu(self.conv1(x))
@@ -75,7 +75,7 @@ class SimpleConv(nn.Module):
         x = x.view(-1, self.linear_size)
         # x = self._dropout(x, dropout_mask, dropout_rate, 0)
         x = F.elu(self.fc1(x))
-        x = self._dropout(x, dropout_mask, dropout_rate, 0)
+        x = self._dropout(x, dropout_mask, dropout_rate, 1)
         x = self.fc2(x)
         return x
 
