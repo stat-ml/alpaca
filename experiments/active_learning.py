e2bca33be0b696bc7ed1da97b16a66fbf44eed2b
mephody-bro <kirill.fedyanin@gmail.com>
2020-01-28 13:27:35 +0300

    Load own dataset

--- experiments/active_learning.py
+++ experiments/active_learning.py
@@ -24,34 +24,6 @@ torch.cuda.set_device(1)
 torch.backends.cudnn.benchmark = True
 
 
-# dataset = build_dataset('cifar_10', val_size=10_000)
-# x_set, y_set = dataset.dataset('train')
-# x_val, y_val = dataset.dataset('val')
-#
-# x_set = np.array(x_set - 128)/128
-# x_val = (x_val - 128)/128
-#
-# train_ds = TensorDataset(torch.Tensor(x_set).view(-1, 3, 32, 32), torch.LongTensor(y_set))
-# val_ds = TensorDataset(torch.Tensor(x_val).view(-1, 3, 32, 32), torch.LongTensor(y_val))
-#
-# data = ImageDataBunch.create(train_ds, val_ds, bs=512)
-# print(type(data))
-
-# import pdb; pdb.set_trace()
-
-# path = untar_data(URLs.CIFAR)
-# ds_tfms = ([*rand_pad(4, 32), flip_lr(p=0.5)], [])
-# data = ImageDataBunch.from_folder(path, valid='test', ds_tfms=ds_tfms, bs=512)  #.normalize(cifar_stats)
-
-#
-# model = AnotherConv()
-# # model = simple_cnn((3, 16, 16, 10))
-#
-# loss_func = torch.nn.CrossEntropyLoss()
-# learn = Learner(data, model, metrics=accuracy, loss_func=loss_func).to_fp16()
-# learn.fit_one_cycle(20, 3e-3, wd=0.4, div_factor=10, pct_start=0.5)
-
-
 class ArrayImageList(ImageList):
     @classmethod
     def from_numpy(cls, numpy_array):
@@ -66,14 +38,35 @@ class ArrayImageList(ImageList):
         return Image(n)
 
 
-x = np.random.rand(50, 3, 224, 224)
-y = np.random.randint(0, 9, 50)
+dataset = build_dataset('cifar_10', val_size=10_000)
+x_set, y_set = dataset.dataset('train')
+x_val, y_val = dataset.dataset('val')
 
-data = (ArrayImageList.from_numpy(x)
-        .split_by_idx(range(30, 50))
-        .label_from_array(y)
-        .databunch(bs=10))
+x_set = ((x_set - 128)/128)
+x_val = ((x_val - 128)/128)
 
-learn = cnn_learner(data, models.resnet18)
-learn.fit(10)
+x = np.concatenate((x_set, x_val)).reshape((-1, 3, 32, 32))
+y = np.concatenate((y_set, y_val))
+train_idxs = range(len(x_set))
+val_idxs = range(len(x_set), len(x_set) + len(x_val))
+
+train_ds = TensorDataset(torch.Tensor(x_set).view(-1, 3, 32, 32), torch.LongTensor(y_set))
+val_ds = TensorDataset(torch.Tensor(x_val).view(-1, 3, 32, 32), torch.LongTensor(y_val))
+
+data = ImageDataBunch.create(train_ds, val_ds, bs=512)
+# print(type(data))
+#
+
+# data = (ArrayImageList.from_numpy(x)
+#         .split_by_idxs(train_idxs, val_idxs)
+#         .label_from_array(y)
+#         .databunch(bs=128))
+#
+
+model = AnotherConv()
+# model = simple_cnn((3, 16, 16, 10))
+
+loss_func = torch.nn.CrossEntropyLoss()
+learn = Learner(data, model, metrics=accuracy, loss_func=loss_func).to_fp16()
+learn.fit_one_cycle(20, 3e-3, wd=0.4, div_factor=10, pct_start=0.5)
 
