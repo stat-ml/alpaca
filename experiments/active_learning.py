341b6911e3401ae28d8c209f98b215f72ac2cb3d
mephody-bro <kirill.fedyanin@gmail.com>
2020-01-28 16:09:46 +0300

    Active learning!

--- experiments/active_learning.py
+++ experiments/active_learning.py
@@ -25,6 +25,13 @@ torch.cuda.set_device(1)
 torch.backends.cudnn.benchmark = True
 
 
+total_size = 60_000
+val_size = 10_000
+start_size = 5_000
+step_size = 1_000
+steps = 5
+
+
 class ImageArrayDS(Dataset):
     def __init__(self, images, labels, tfms=None):
         self.images = torch.FloatTensor(images)
@@ -40,7 +47,7 @@ class ImageArrayDS(Dataset):
     def __len__(self):
         return len(self.images)
 
-
+# Load data
 dataset = build_dataset('cifar_10', val_size=10_000)
 x_set, y_set = dataset.dataset('train')
 x_val, y_val = dataset.dataset('val')
@@ -49,22 +56,39 @@ shape = (-1, 3, 32, 32)
 x_set = ((x_set - 128)/128).reshape(shape)
 x_val = ((x_val - 128)/128).reshape(shape)
 
+# Start data split
+x_pool, x_train, y_pool, y_train = train_test_split(x_set, y_set, test_size=start_size, stratify=y_set)
 
 train_tfms = [*rand_pad(4, 32), flip_lr(p=0.5)]
-train_ds = ImageArrayDS(x_set, y_set, train_tfms)
-val_ds = ImageArrayDS(x_val, y_val)
-
-data = ImageDataBunch.create(train_ds, val_ds, bs=512)
-
-model = AnotherConv()
-# model = simple_cnn((3, 16, 16, 10))
-
-callbacks = [
-    partial(EarlyStoppingCallback, monitor='valid_loss', min_delta=0.001, patience=3)]
 loss_func = torch.nn.CrossEntropyLoss()
-learner = Learner(data, model, metrics=accuracy, loss_func=loss_func, callback_fns=callbacks).to_fp16()
-learner.fit_one_cycle(100, 3e-3, wd=0.4, div_factor=10, pct_start=0.5)
-
-learner.recorder.plot_losses(); plt.show()
+# model = AnotherConv()
+model = simple_cnn((3, 16, 16, 10))
 
 
+train_losses = []
+val_losses = []
+val_accuracy = []
+
+for i in range(steps):
+    print(f"Epoch {i+1}, train size: {len(x_train)}")
+    train_ds = ImageArrayDS(x_train, y_train, train_tfms)
+    val_ds = ImageArrayDS(x_val, y_val)
+
+    data = ImageDataBunch.create(train_ds, val_ds, bs=256)
+
+    # callbacks = [partial(EarlyStoppingCallback, monitor='valid_loss', min_delta=0.001, patience=3)]
+    callbacks = []
+    learner = Learner(data, model, metrics=accuracy, loss_func=loss_func, callback_fns=callbacks) #.to_fp16()
+    # learner.fit_one_cycle(100, 3e-3, wd=0.4, div_factor=10, pct_start=0.5)
+    learner.fit(5, 3e-3, wd=0.2)
+
+    train_losses.append(learner.recorder.losses[-1])
+    val_losses.append(learner.recorder.val_losses[-1])
+    val_accuracy.append(learner.recorder.metrics[-1][0].item())
+
+    x_pool, x_add, y_pool, y_add = train_test_split(x_pool, y_pool, test_size=step_size, stratify=y_pool)
+    x_train = np.concatenate((x_train, x_add))
+    y_train = np.concatenate((y_train, y_add))
+
+
+import pdb; pdb.set_trace()
