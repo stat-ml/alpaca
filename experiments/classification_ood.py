a4edbda42452ba376a211239e945b22bff8b88b7
mephody-bro <kirill.fedyanin@gmail.com>
2020-02-17 16:00:15 +0300

    OOD mnist/fashion

--- experiments/classification_ood.py
+++ experiments/classification_ood.py
@@ -11,7 +11,7 @@ import pandas as pd
 import torch
 import torch.nn.functional as F
 
-from fastai.vision import (rand_pad, flip_lr, ImageDataBunch, Learner, accuracy, Image)
+from fastai.vision import (ImageDataBunch, Learner, accuracy)
 from fastai.callbacks import EarlyStoppingCallback
 
 sys.path.append('..')
@@ -26,12 +26,12 @@ from sklearn.metrics import roc_auc_score, roc_curve
 from uncertainty_estimator.masks import build_mask
 from experiment_setup import build_estimator
 from active_learning.simple_update import entropy
-from dataloader.builder import build_dataset
+from experiments.utils.visual_datasets import prepare_fashion_mnist
 
 torch.cuda.set_device(0)
 
 config_mnist = {
-    'val_size': 10_000,
+    'val_size': 5_000,
     'train_size': 5_000,
     'model_type': 'simple_conv',
     'batch_size': 256,
@@ -45,6 +45,7 @@ config_mnist = {
     'repeats': 5,
     'name': 'MNIST',
     'prepare_dataset': prepare_mnist,
+    'alternative_dataset': prepare_fashion_mnist,
 }
 
 config_cifar = deepcopy(config_mnist)
@@ -72,6 +73,8 @@ def benchmark_ood(config):
         _, x_train, _, y_train = train_test_split(
             x_set, y_set, test_size=config['train_size'], stratify=y_set)
 
+        _, _, x_alt, y_alt, _ = config['alternative_dataset'](config)
+
         train_ds = ImageArrayDS(x_train, y_train, train_tfms)
         val_ds = ImageArrayDS(x_val, y_val)
         data = ImageDataBunch.create(train_ds, val_ds, bs=config['batch_size'])
@@ -84,34 +87,35 @@ def benchmark_ood(config):
         learner = Learner(data, model, metrics=accuracy, loss_func=loss_func, callback_fns=callbacks)
         learner.fit(config['epochs'], config['start_lr'], wd=config['weight_decay'])
 
-        images = torch.FloatTensor(x_val).cuda()
+        original_images = torch.FloatTensor(x_val)
+        alt_images = torch.FloatTensor(x_alt)
+        images = torch.cat((original_images, alt_images)).cuda()
+        y = np.array([0]*len(original_images) + [1]*len(alt_images))
 
         probabilities = F.softmax(model(images), dim=1).detach().cpu().numpy()
-        predictions = np.argmax(probabilities, axis=-1)
 
         for name in config['estimators']:
             ue = calc_ue(model, images, probabilities, name, config['nn_runs'])
-            mistake = 1 - (predictions == y_val).astype(np.int)
 
-            roc_auc = roc_auc_score(mistake, ue)
+            roc_auc = roc_auc_score(y, ue)
             print(name, roc_auc)
             results.append((name, roc_auc))
 
             if i == config['repeats'] - 1:
-                fpr, tpr, thresholds = roc_curve(mistake, ue, pos_label=1)
+                fpr, tpr, thresholds = roc_curve(y, ue, pos_label=1)
                 plt.plot(fpr, tpr, label=name, alpha=0.8)
 
     dir = Path(ROOT_DIR) / 'experiments' / 'data' / 'ood'
-    plt.title(f"{config['name']} uncertainty ROC")
+    plt.title(f"{config['name']} ood detection ROC")
     plt.legend()
-    plt.savefig(dir / f"roc_{config['name']}_{config['train_size']}")
+    plt.savefig(dir / f"ood_roc_{config['name']}_{config['train_size']}")
     # plt.show()
 
     plt.figure(figsize=(10, 8))
-    plt.title(f"{config['name']} uncertainty ROC-AUC")
+    plt.title(f"{config['name']} ood detection ROC-AUC")
     df = pd.DataFrame(results, columns=['Estimator type', 'ROC-AUC score'])
     sns.boxplot('Estimator type', 'ROC-AUC score', data=df)
-    plt.savefig(dir / f"boxplot_{config['name']}_{config['train_size']}")
+    plt.savefig(dir / f"ood_boxplot_{config['name']}_{config['train_size']}")
     # plt.show()
 
 
@@ -129,26 +133,8 @@ def calc_ue(model, images, probabilities, estimator_type='max_prob', nn_runs=100
     return ue
 
 
-def prepare_notmnist(config):
-    dataset = build_dataset('not_mnist', val_size=config['val_size'])
-    x_set, y_set = dataset.dataset('train')
-    x_val, y_val = dataset.dataset('val')
-    print(x_set.shape)
-
-    shape = (-1, 1, 28, 28)
-    x_set = ((x_set - 128) / 128).reshape(shape)
-    x_val = ((x_val - 128) / 128).reshape(shape)
-
-    train_tfms = []
-
-    return x_set, y_set, x_val, y_val, train_tfms
-
-
 if __name__ == '__main__':
-    print('hello')
-    config = configs[0]
-    prepare_notmnist(config)
-    # for config in configs:
-    #     print(config)
-    #     benchmark_ood(config)
+    for config in configs:
+        print(config)
+        benchmark_ood(config)
 
