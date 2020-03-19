32d45eac6582b699a7537786b316745456b951bb
mephody-bro <kirill.fedyanin@gmail.com>
2020-03-19 17:32:56 +0300

    Fix setups

--- alpaca/dataloader/utils.py
+++ alpaca/dataloader/utils.py
@@ -17,7 +17,7 @@ def loader(x, y, batch_size=128, task='regression'):
         ds = TensorDataset(torch.DoubleTensor(x), torch.DoubleTensor(y))
     elif task == 'classification':
         ds = TensorDataset(torch.DoubleTensor(x), torch.LongTensor(y))
-    _loader = DataLoader(ds, batch_size=batch_size)
+    else:
+        raise ValueError('Unknown task: should be "classificaiton" or "regression"')
+    _loader = DataLoader(ds, batch_size=batch_size, num_workers=4)
     return _loader
-
-
