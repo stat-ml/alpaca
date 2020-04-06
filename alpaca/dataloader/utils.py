fa6d85e03204852591c7de602bf2d6307f7d2a12
mephody-bro <kirill.fedyanin@gmail.com>
2020-04-06 11:49:22 +0300

    jjjjjjjjjjjjjjjjjjjjjjjjjjj

--- alpaca/dataloader/utils.py
+++ alpaca/dataloader/utils.py
@@ -12,12 +12,12 @@ def scale(train, val):
 
 
 # Set initial datas
-def loader(x, y, batch_size=128, task='regression'):
+def loader(x, y, batch_size=128, task='regression', shuffle=False):
     if task == 'regression':
         ds = TensorDataset(torch.DoubleTensor(x), torch.DoubleTensor(y))
     elif task == 'classification':
         ds = TensorDataset(torch.DoubleTensor(x), torch.LongTensor(y))
     else:
         raise ValueError('Unknown task: should be "classificaiton" or "regression"')
-    _loader = DataLoader(ds, batch_size=batch_size, num_workers=4)
+    _loader = DataLoader(ds, batch_size=batch_size, num_workers=4, shuffle=shuffle)
     return _loader
