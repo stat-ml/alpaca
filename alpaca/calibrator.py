a8a8e4910c57270f1049c6d31b5fff35f2ead001
Михаил Терешкин <tereshkin.ma@phystech.edu>
2020-09-06 16:58:10 +0300

    Change names of methods

---
+++ alpaca/calibrator.py
@@ -0,0 +1,232 @@
+from sklearn.metrics import accuracy_score
+import numpy as np
+import math
+from torch.nn import functional as f
+import torch
+from torch import nn, optim
+from scipy.special import softmax
+import pandas as pd
+
+
+def _split_into_bins(n_bins, probs, labels):
+    bins = []
+    true_labels_for_bins = []
+
+    for i in range(n_bins):
+        bins.append([])
+        true_labels_for_bins.append([])
+
+    for j in range(len(labels)):
+        max_p = max(probs[j])
+        for i in range(n_bins):
+            if i / n_bins < max_p and max_p <= (i + 1) / n_bins:
+                bins[i].append((probs[j]))
+                true_labels_for_bins[i].append(labels[j])
+    return np.array(bins, dtype=object), np.array(true_labels_for_bins, dtype=object)
+
+
+def compute_ece(n_bins, probs, labels, len_dataset):
+    bins, true_labels_for_bins = _split_into_bins(n_bins, probs, labels)
+    bins = list(filter(None, bins))
+    true_labels_for_bins = list(filter(None, true_labels_for_bins))
+    ece = torch.zeros(1)
+    for i in range(len(bins)):
+        softmaxes = torch.from_numpy(np.array(bins[i]))
+        confidences, predictions = torch.max(softmaxes, dim=1)
+        accuracy = accuracy_score(true_labels_for_bins[i], predictions)
+        confidence = torch.sum(confidences) / len(bins[i])
+        ece += len(bins[i]) * torch.abs(accuracy - confidence) / len_dataset
+    return ece
+
+
+def _split_into_classes(labels, probs):
+    class_probs = []
+    dict_class_probs = {}
+    n_classes = np.shape(probs)[1]
+    for i in range(n_classes):
+        class_probs.append([])
+    for ind, label in enumerate(labels):
+        for i in range(n_classes):
+            if label == i:
+                class_probs[i].append(probs[ind])
+    for i in range(n_classes):
+        dict_class_probs[i] = class_probs[i]
+    return dict_class_probs
+
+
+def compute_sce(nbins, probs, labels):
+    ece_values_for_each_class = []
+    dict_class_probs = _split_into_classes(labels, probs)
+    for item in dict_class_probs.keys():
+        ece_values_for_each_class.append(
+            compute_ece(nbins, dict_class_probs[item], np.array([item] * np.shape(dict_class_probs[item])[0]),
+                        len(labels)))
+    return sum(ece_values_for_each_class) / len(dict_class_probs.keys())
+
+
+def _split_into_ranges(R, probs, labels):
+    N = len(probs)
+    bins = []
+    true_labels = []
+    for i in range(R):
+        bins.append([])
+        true_labels.append([])
+    for j in range(R):
+        for i in range(j * math.floor(N / R), (j + 1) * math.floor(N / R)):
+            bins[j].append(probs[i])
+            true_labels[j].append(labels[i])
+    return np.array(bins, dtype=object), np.array(true_labels, dtype=object)
+
+
+def compute_ace(R, labels, probs):
+    dict_class_probs = _split_into_classes(labels, probs)
+    summa = 0
+    for item in dict_class_probs.keys():
+        class_labels = np.array([item] * np.shape(dict_class_probs[item])[0])
+        class_probs = dict_class_probs[item]
+        bins, true_labels = _split_into_ranges(R, class_probs, class_labels)
+        for binn, bin_labels in zip(bins, true_labels):
+            conf_array, predictions = torch.max(torch.from_numpy(binn), dim=1)
+            accuracy = accuracy_score(bin_labels, predictions.numpy())
+            confidence = torch.sum(conf_array) / len(conf_array)
+            substraction = abs(accuracy - confidence)
+            summa += substraction
+    ACE = summa / (len(dict_class_probs.keys()) * R)
+    return ACE
+
+
+def _choose_data(threshold, labels, probs):
+    arr = torch.max(torch.from_numpy(np.array(probs)), dim=1)[0]
+    arr.numpy()
+    arr_with_indices = list(enumerate(arr))
+    arr_with_indices.sort(key=lambda x: x[1])
+    thr_array = []
+    for pair in arr_with_indices:
+        if pair[1] > threshold:
+            thr_array.append(pair)
+    indices = []
+    for pair in thr_array:
+        indices.append(pair[0])
+    chosen_labels = labels[indices]
+    chosen_probs = probs[indices]
+    return chosen_labels, chosen_probs
+
+
+def compute_tace(threshold, labels, probs, R):
+    if isinstance(labels, pd.DataFrame) or isinstance(labels, pd.Series):
+        labels = labels.to_numpy()
+    chosen_labels, chosen_probs = _choose_data(threshold, labels, probs)
+    return compute_ace(R, chosen_labels, chosen_probs)
+
+
+class ModelWithTempScaling(nn.Module):
+
+    def __init__(self, model, logits, labels):
+        super(ModelWithTempScaling, self).__init__()
+        self.model = model
+        self.temperature = nn.Parameter(torch.ones(1))
+        self.logits = logits
+        self.labels = labels
+
+    def forward(self, input):
+        logits = self.model(input)
+        return torch.true_divide(logits, self.temperature)
+
+    def scaling(self, lr=0.01, max_iter=50):
+        nll = nn.CrossEntropyLoss()
+        optimizer = optim.LBFGS([self.temperature], lr=lr, max_iter=max_iter)
+
+        def eval():
+            loss = nll(torch.true_divide(self.logits, self.temperature), self.labels)
+            loss.backward()
+            return loss
+
+        optimizer.step(eval)
+        return self
+
+
+class ModelWithVectScaling(nn.Module):
+    def __init__(self, model, logits, labels):
+        super(ModelWithVectScaling, self).__init__()
+        self.model = model
+        self.logits = logits.float()
+        self.labels = labels
+        self.W_and_b = nn.Parameter(
+            torch.cat((torch.ones(logits.shape[1]), torch.zeros(logits.shape[1])), dim=0))
+
+    def forward(self, input):
+        logits = self.model(input)
+        return self.scaling_logits(logits)
+
+    def scaling_logits(self, logits):
+        W = torch.diag(self.W_and_b[:logits.shape[1]])
+        b = self.W_and_b[logits.shape[1]:]
+        b = b.unsqueeze(0).expand(logits.shape[0], -1)
+        return torch.mm(logits, W) + b
+
+    def scaling(self, lr=0.00001, max_iter=3500):
+        nll = nn.CrossEntropyLoss()
+        optimizer = optim.LBFGS([self.W_and_b], lr=lr, max_iter=max_iter)
+
+        def eval():
+            loss = nll(self.vector_scaling_logits(self.logits), self.labels)
+            loss.backward()
+            return loss
+
+        optimizer.step(eval)
+        return self
+
+
+class ModelWithMatrScaling(nn.Module):
+    def __init__(self, model, logits, labels):
+        super(ModelWithMatrScaling, self).__init__()
+        self.model = model
+        self.logits = logits.float()
+        self.labels = labels
+        self.W = nn.Parameter(torch.diag(torch.ones(logits.shape[1])))
+        self.b = nn.Parameter(torch.zeros(logits.shape[1]))
+
+    def forward(self, input):
+        logits = self.model(input)
+        return self.scaling_logits(logits)
+
+    def scaling_logits(self, logits):
+        self.b.unsqueeze(0).expand(logits.shape[0], -1)
+        return torch.mm(logits, self.W) + self.b
+
+    def scaling(self, lr=0.001, max_iter=100):
+        nll = nn.CrossEntropyLoss()
+        optimizer = optim.LBFGS([self.W, self.b], lr=lr, max_iter=max_iter)
+
+        def eval():
+            loss = nll(self.matrix_scaling_logits(self.logits), self.labels)
+            loss.backward()
+            return loss
+
+        optimizer.step(eval)
+        return self
+
+
+def binary_histogram_binning(num_bins, probs, labels, probs_to_calibrate):
+    bins = np.linspace(0, 1, num=num_bins)
+    indexes_list = np.digitize(probs, bins) - 1
+    theta = np.zeros(num_bins)
+    for i in range(len(bins)):
+        binn = (indexes_list == i)
+        binn_len = np.sum(binn)
+        if binn_len != 0:
+            theta[i] = np.sum(labels[binn]) / binn_len
+        else:
+            theta[i] = bins[i]
+    return list(map(lambda x: theta[np.digitize(x, bins) - 1], probs_to_calibrate))
+
+
+def multiclass_histogram_binning(num_bins, logits, labels, logits_to_calibrate):
+    probs = softmax(logits, axis=1)
+    probs_to_calibrate = softmax(logits_to_calibrate, axis=1)
+    binning_res = []
+    for k in range(np.shape(probs)[1]):
+        binning_res.append(binary_histogram_binning(num_bins, probs[:, k], labels == k, probs_to_calibrate[:, k]))
+    binning_res = np.vstack(binning_res).T
+    cal_confs = binning_res / (np.sum(binning_res, axis=1)[:, None])
+    return cal_confs
