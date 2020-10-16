8f3d986df71e753a2d5e9b8521513e9c036fe5da
Rasul Karimov <yoatom96@hotmail.com>
2020-10-16 11:49:44 +0400

    rm old tests

--- tests/uncertainty_estimator/test_masks.py
+++
@@ -1,9 +0,0 @@
-import torch
-from alpaca.uncertainty_estimator.masks import BasicBernoulliMask
-
-
-def test_basic_mask():
-    mask = BasicBernoulliMask()
-    x = torch.Tensor(range(6))
-    result = mask(x, dropout_rate=0.33)
-    assert result.shape == (6,)
