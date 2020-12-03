21979caa792ab7bab0bf39751d88e2ab4862f059
kirill.fedyanin <kirill.fedyanin@gmail.com>
2020-12-03 17:06:04 +0300

    Return dpp

--- alpaca/ue/masks.py
+++ alpaca/ue/masks.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 import abc
 from typing import Union
 import torch
+import numpy as np
+from scipy.optimize import root_scalar
 
 from alpaca.utils.functions import corrcoef, mc_probability, cov
 from dppy.finite_dpps import FiniteDPP
@@ -60,27 +63,11 @@ class BaseMask(metaclass=abc.ABCMeta):
         pass
 
 
-class MaskLayered(BaseMask):
-    __doc__ = r"""
-    The base class for nn layered masks
-    """
+class DecorrelationMask(BaseMask):
+    pass
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.layer_correlations: dict = dict()
-        self.norm: dict = dict()
-
-    @abc.abstractmethod
-    def reset(self):
-        """
-        Resets layers info/stat
-        """
-        pass
-
-    def reset(self):
-        self.layer_correlations = None
-        self._init_run = True
-
+class LeverageScoreMask(BaseMask):
+    pass
 
 class BasicBernoulliMask(BaseMask):
     """
@@ -121,176 +108,46 @@ class BasicBernoulliMask(BaseMask):
         return res
 
 
-class DecorrelationMask(MaskLayered):
-    """
-    TODO:
-    """
-
-    _name_collection = {"decorrelating"}
-
-    def __init__(
-        self,
-        *,
-        scaling: bool = False,
-        ht_norm: bool = False,
-        eps: float = 1e-8,
-    ):
-        super().__init__()
-        self.scaling = scaling  # use adaptive scaling before softmax
+ATTEMPTS = 5
+class DPPMask:
+    def __init__(self, ht_norm=True, covariance=False):
+        self.dpps = {}
+        self.layer_correlations = {}  # keep for debug purposes # Flag for uncertainty estimator to make first run without taking the result
+        self.dry_run = True
         self.ht_norm = ht_norm
-        self.eps = eps
-
-    def __call__(
-        self,
-        x: torch.Tensor,
-        dropout_rate: Union[torch.Tensor, float] = 0.5,
-        *,
-        is_train=True,
-    ) -> torch.Tensor:
-        mask_len = x.size(-1)
-        k = int(mask_len * (1.0 - dropout_rate))
-
-        if self._init_run is True and is_train is False:
-            self._init_run = False
-            return self._init_layers(x, mask_len, k)
-
-        mask = torch.zeros(mask_len, dtype=x.dtype, device=x.device)
-        inds = torch.multinomial(self.layer_correlation, k, replacement=False)
-
-        if self.ht_norm is True:
-            mask[inds] = self.norm[inds]
-        else:
-            mask[inds] = torch.Tensor([1 / (1 - dropout_rate)]).to(dtype=x.dtype)
-
-        return mask.expand_as(x)
-
-    def _init_layers(self, x: torch.Tensor, mask_len: int, k: int) -> torch.Tensor:
-        noise = torch.rand(*x.shape, dtype=x.dtype, device=x.device) * self.eps
-        corrs = torch.sum(torch.abs(corrcoef((x + noise).transpose(1, 0))), dim=1)
-        scores = torch.reciprocal(corrs)
-
-        if self.scaling:
-            scores = (
-                4.0 * scores / torch.max(scores)
-            )  # TODO: remove hard coding or annotate
-        self.layer_correlation = torch.nn.functional.softmax(scores, dim=-1)
-
-        if self.ht_norm:
-            # Horvitz-Thopson normalization (1 / marginal_prob for each element)
-            probabilities = self.layer_correlation
-            samples = max(1000, 4 * x.size(-1))  # TODO: why? (explain 1000)
-            self.norm = torch.reciprocal(mc_probability(probabilities, k, samples))
-
-        # Initially we should pass identity mask,
-        # otherwise we won't get right correlations for all layers
-        return torch.ones(mask_len, dtype=x.dtype, device=x.device)
-
-
-class DecorrelationMaskScaled(MaskLayered):
-    """
-    TODO:
-    """
-
-    _name_collection = {"decorrelating_sc"}
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.scaling = True
-
-
-class LeverageScoreMask(MaskLayered):
-    """
-    TODO:
-    """
-
-    _name_collection = {"leveragescoremask"}
-
-    def __init__(
-        self,
-        *,
-        ht_norm: bool = True,
-        lambda_: int = 1,
-        covariance: bool = False,
-    ):
-        super().__init__()
-        self.ht_norm = ht_norm
-        self.lambda_ = lambda_
+        self.norm = {}
         self.covariance = covariance
+        self._init_run = True
 
-    def __call__(
-        self,
-        x: torch.Tensor,
-        dropout_rate: Union[torch.Tensor, float] = 0.5,
-        *,
-        is_train=True,
-    ) -> torch.Tensor:
-        mask_len = x.shape[-1]
-        k = int(mask_len * (1 - dropout_rate))
+    def __call__(self, x, dropout_rate=0.5, layer_num=0, is_train=True):
+        if layer_num not in self.layer_correlations:
+            # warm-up, generatign correlations masks
+            x_matrix = x.cpu().numpy()
 
-        if self._init_run is True and is_train is False:
-            self._init_run = False
-            return self._init_layers(x, mask_len, k)
+            self.x_matrix = x_matrix
+            micro = 1e-12
+            x_matrix += np.random.random(x_matrix.shape) * micro  # for computational stability
+            if self.covariance:
+                L = np.cov(x_matrix.T)
+            else:
+                L = np.corrcoef(x_matrix.T)
 
-        mask = torch.zeros(mask_len, device=x.device)
-        ids = torch.multinomial(self.layer_correlations, k, replacement=False)
+            self.dpps[layer_num] = FiniteDPP('likelihood', **{'L': L})
+            self.layer_correlations[layer_num] = L
 
-        if self.ht_norm:
-            mask[ids] = self.norm[ids]
-        else:
-            mask[ids] = 1 / (1 - dropout_rate)
+            if self.ht_norm:
+                L = torch.DoubleTensor(L).cuda()
+                I = torch.eye(len(L)).double().cuda()
+                K = torch.mm(L, torch.inverse(L + I))
 
-        return mask.expand_as(x)
+                self.norm[layer_num] = torch.reciprocal(torch.diag(K))  # / len(correlations)
+                self.L = L
+                self.K = K
 
-    def _init_layers(self, x: torch.Tensor, mask_len: int, k: int):
-        if self.covariance:
-            K = cov(x.transpose(1, 0))
-        else:
-            K = corrcoef(x.transpose(1, 0))
-        identity = torch.eye(K.size(0))
-        leverages_matrix = K @ torch.inverse(K + self.lambda_ * identity)
-        probabilities = torch.diagonal(leverages_matrix)
-        probabilities = probabilities / torch.sum(probabilities)
-        self.layer_correlations = probabilities
-
-        if self.ht_norm:
-            # Horvitz-Thopson normalization (1 / marginal_prob for each element)
-            probabilities = self.layer_correlations
-            samples = max(1000, 4 * x.size(-1))  # TODO: why? (explain 1000)
-            self.norm = torch.reciprocal(mc_probability(probabilities, k, samples))
-
-        # Initially we should pass identity mask,
-        # otherwise we won't get right correlations for all layers
-        return torch.ones(mask_len, device=x.device)
-
-
-class LeverageScoreMaskCov(LeverageScoreMask):
-    _name_collection = {"cov_leverages"}
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.ht_norm = True
-        self.lambda_ = 1
-        self.covariance = True
-
-
-class DPPMask(MaskLayered):
-    def __init__(self, ht_norm: bool = False, covariance: bool = False):
-        self.ht_norm = ht_norm
-        self.covariance = covariance
-
-    def __call__(
-        self,
-        x: torch.Tensor,
-        dropout_rate: Union[torch.Tensor, float] = 0.5,
-        *,
-        is_train=True,
-    ) -> torch.Tensor:
-        if self._init_run is True and is_train is False:
-            self._init_run = False
-            return self._init_layers(x)
+            return x.data.new(x.data.size()[-1]).fill_(1)
 
         # sampling nodes ids
-        dpp = self.dpps
+        dpp = self.dpps[layer_num]
 
         for _ in range(ATTEMPTS):
             dpp.sample_exact()
@@ -305,27 +162,167 @@ class DPPMask(MaskLayered):
         else:
             mask[ids] = mask_len / len(ids)
 
-        return x.data.new(mask)
+        # return x.data.new(mask)
+        return mask.to(x.device)
 
-    def _init_layers(self, x: torch.Tensor, eps: float = 1e-12):
-        x += torch.rand(x.shape) * eps
-        if self.covariance:
-            L = cov(x_matrix.transpose(0, 1))
-        else:
-            L = corrcoef(x_matrix.transpose(0, 1))
+    def reset(self):
+        self.layer_correlations = {}
 
-        self.dpps = FiniteDPP("likelihood", **{"L": L})
-        self.layer_correlations[layer_num] = L
+
+def get_nu(eigen_values, k):
+    """
+    Get tilting coefficient to correctly approximate k-dpp marginal probabilities
+    See amblard2018
+    :param eigen_values: eigen values of L (likelihood) matrix for dpp
+    :param k: how much samples do we plan to take
+    :return: tilting coefficient
+    """
+    values = eigen_values + 1e-14
+    def point(nu):
+        exp_nu = np.exp(nu)
+        expect = np.sum([val*exp_nu / (1 + exp_nu*val) for val in values])
+        return expect - k
+
+    try:
+        solution = root_scalar(point, bracket=[-10., 10.])
+        assert solution.converged
+        return solution.root
+    except (ValueError, AssertionError):
+        raise ValueError('k-dpp: Probably too small matrix rank for the k')
+
+
+class KDPPMask:
+    def __init__(self, noise_level=None, tol_level=1e-3, ht_norm=True, covariance=False):
+        self.layer_correlations = {}
+        self.dry_run = True
+        self.dpps = {}
+        self.ranks = {}
+        self.ranks_history = defaultdict(list)
+        self.noise_level = noise_level
+        self.tol_level = tol_level
+
+        self.ht_norm = ht_norm
+        self.norm = {}
+
+        self.covariance = covariance
+
+    def _rank(self, dpp=None, eigen_values=None):
+        if eigen_values is None:
+            eigen_values = dpp.L_eig_vals
+        rank = np.count_nonzero(eigen_values > self.tol_level)
+        return rank
+
+    def __call__(self, x, dropout_rate=0.5, layer_num=0):
+        mask_len = x.shape[-1]
+        k = int(mask_len * (1-dropout_rate))
+
+        if layer_num not in self.layer_correlations:
+            x_matrix = x.cpu().numpy()
+
+            if self.covariance:
+                L = np.cov(x_matrix.T)
+            else:
+                L = np.corrcoef(x_matrix.T)
+
+            if self.noise_level is not None:
+                L += self.noise_level * np.eye(len(L))
+
+            if not self.ht_norm:
+                self.dpps[layer_num] = FiniteDPP('likelihood', **{'L': L})
+                self.dpps[layer_num].sample_exact()  # to trigger eig values generation
+            else:
+                eigen_values = np.linalg.eigh(L)[0]
+                "Get tilted k-dpp, see amblard2018"
+                nu = get_nu(eigen_values, k)
+                I = torch.eye(len(L)).to(x.device)
+                L_tilted = np.exp(nu) * torch.DoubleTensor(L).to(x.device)
+                K_tilted = torch.mm(L_tilted, torch.inverse(L_tilted + I)).double()
+                self.dpps[layer_num] = FiniteDPP('correlation', **{"K": K_tilted.detach().cpu().numpy()})
+                self.norm[layer_num] = torch.reciprocal(torch.diag(K_tilted))
+                self.L = L_tilted
+                self.K = K_tilted
+
+            # Keep data for debugging
+            self.layer_correlations[layer_num] = L
+            mask = torch.ones(mask_len).double().to(x.device)
+
+            return mask
+
+        mask = torch.zeros(mask_len).double().to(x.device)
+        ids = self.dpps[layer_num].sample_exact_k_dpp(k)
 
         if self.ht_norm:
-            L = torch.DoubleTensor(L).cuda()
-            I = torch.eye(len(L)).double().cuda()
-            K = torch.mm(L, torch.inverse(L + I))
+            mask[ids] = self.norm[layer_num][ids]
+        else:
+            mask[ids] = mask_len / len(ids)
 
-            self.norm[layer_num] = torch.reciprocal(
-                torch.diag(K)
-            )  # / len(correlations)
-            self.L = L
-            self.K = K
+        return mask
 
-        return x.data.new(x.data.size()[-1]).fill_(1)
+    def reset(self):
+        self.layer_correlations = {}
+
+
+# class DPPMask(BaseMask):
+#     def __init__(self, ht_norm: bool = False, covariance: bool = False):
+#         super().__init__()
+#         self.ht_norm = ht_norm
+#         self.covariance = covariance
+#
+#         self.layer_correlations: dict = dict()
+#         self.norm: dict = dict()
+#
+#     def __call__(
+#         self,
+#         x: torch.Tensor,
+#         dropout_rate: Union[torch.Tensor, float] = 0.5,
+#         *,
+#         is_train=True,
+#     ) -> torch.Tensor:
+#         if self._init_run is True and is_train is False:
+#             self._init_run = False
+#             return self._init_layers(x)
+#
+#         # sampling nodes ids
+#         dpp = self.dpps
+#
+#         for _ in range(ATTEMPTS):
+#             dpp.sample_exact()
+#             ids = dpp.list_of_samples[-1]
+#             if len(ids):  # We should retry if mask is zero-length
+#                 break
+#
+#         mask_len = x.shape[-1]
+#         mask = torch.zeros(mask_len).double().cuda()
+#         if self.ht_norm:
+#             mask[ids] = self.norm[layer_num][ids]
+#         else:
+#             mask[ids] = mask_len / len(ids)
+#
+#         return x.data.new(mask)
+#
+#     def reset(self):
+#         self.layer_correlations = None
+#         self._init_run = True
+#
+#     def _init_layers(self, x: torch.Tensor, eps: float = 1e-12):
+#         x += torch.rand(x.shape) * eps
+#         if self.covariance:
+#             L = cov(x_matrix.transpose(0, 1))
+#         else:
+#             L = corrcoef(x_matrix.transpose(0, 1))
+#
+#         self.dpps = FiniteDPP("likelihood", **{"L": L})
+#         self.layer_correlations[layer_num] = L
+#
+#         if self.ht_norm:
+#             L = torch.DoubleTensor(L).cuda()
+#             I = torch.eye(len(L)).double().cuda()
+#             K = torch.mm(L, torch.inverse(L + I))
+#
+#             self.norm[layer_num] = torch.reciprocal(
+#                 torch.diag(K)
+#             )  # / len(correlations)
+#             self.L = L
+#             self.K = K
+#
+#         return x.data.new(x.data.size()[-1]).fill_(1)
