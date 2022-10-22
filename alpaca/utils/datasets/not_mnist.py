71a9f1a01a413b1fed2c6391a2a78c06fb690550
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-22 07:51:39 +0000

    Adding tarfile member sanitization to extractall()

--- alpaca/utils/datasets/not_mnist.py
+++ alpaca/utils/datasets/not_mnist.py
@@ -36,7 +36,26 @@ class NotMNISTData:
 
         ipdb.set_trace()
         with tarfile.open(data_path, "r:gz") as tar_ref:
-            tar_ref.extractall(cache_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_ref, cache_dir)
 
         base_dir = cache_dir / "notMNIST_small"
 
